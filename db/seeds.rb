6b97fc9ef907439c33c43b37b56d5857ee62f597
chanweiyan <chanweiyan007@gmail.com>
2017-05-22 13:54:00 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.create(email:"101@qq.com", password: 123456, password_confirmation: 123456, is_admin: true)
-puts "新建一个admin用户！"
+# email & email可以改成自己的；
+# password 至少要六位数字
+
+u = User.create(
+                email:"chanweiyan@gmail.com",
+                password: 123456,
+                password_confirmation: 123456,
+                is_admin: true)
+
+u = User.create(
+                email:"397721260@qq.com",
+                password: 123456,
+                password_confirmation: 123456,
+                is_admin: true)
+
+puts "新建2个admin用户！"
