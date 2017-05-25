abbe293277040654919dff05c9e15f1a07c208ca
chanweiyan <chanweiyan007@gmail.com>
2017-05-25 21:42:39 +0800

    add product_details model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524230343) do
+ActiveRecord::Schema.define(version: 20170525133916) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -56,6 +56,13 @@ ActiveRecord::Schema.define(version: 20170524230343) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_details", force: :cascade do |t|
     t.integer  "product_id"
     t.string   "image"
     t.datetime "created_at", null: false
---
+++ test/models/product_detail_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: product_details
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class ProductDetailTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
