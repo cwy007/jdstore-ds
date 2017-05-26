ef4ea5d41a50bcb9585210d0f8aa1f4d669975ac
chanweiyan <chanweiyan007@gmail.com>
2017-05-26 15:50:21 +0800

    implement category image upload

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,12 +2,15 @@
 #
 # Table name: categories
 #
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :integer          not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  category_icon :string
 #
 
 class Category < ApplicationRecord
   has_many :products
+  mount_uploader :category_icon, ImageUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525133916) do
+ActiveRecord::Schema.define(version: 20170526061017) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20170525133916) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "category_icon"
   end
 
   create_table "comments", force: :cascade do |t|
