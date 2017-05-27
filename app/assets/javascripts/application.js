9f594974bd96d8f070592918af2945f86ff6d9c3
chanweiyan <chanweiyan007@gmail.com>
2017-05-27 16:22:05 +0800

    implement navbar cart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,24 @@
 //= require bootstrap-sprockets
 //= require select2
 //= require_tree .
+
+$(document).on("turbolinks:load", function(){
+  /*PJH 1113 begin 鼠标移动到购物车块部弹出购物车下拉*/
+  $("#top-cart .cart-bd").mouseenter(function() {
+    $(this).css("background-color","#FFF").css("border-bottom","none");
+    $("#top-cart .cart-hide").show();
+  });
+  $("#top-cart .cart-bd").mouseleave(function() {
+    $(this).css("background-color","#F9F9F9").css("border-bottom","1px solid #ddd");
+    $("#top-cart .cart-hide").hide();
+  });
+  $("#top-cart .cart-hide").mouseenter(function() {
+    $("#top-cart .cart-bd").css("background-color","#FFF").css("border-bottom","none");
+    $(this).show();
+  });
+  $("#top-cart .cart-hide").mouseleave(function() {
+    $("#top-cart .cart-bd").css("background-color","#F9F9F9").css("border-bottom","1px solid #ddd");
+    $(this).hide();
+  });
+   /*PJH 1113 end 鼠标移动到购物车块部弹出购物车下拉*/
+})
