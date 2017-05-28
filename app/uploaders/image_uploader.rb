662b92c75708ab347b58dfe57e6d4740372cef24
chanweiyan <chanweiyan007@gmail.com>
2017-05-28 21:24:09 +0800

    implement category image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,15 +19,17 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [4000,4000]   #保持原图比例，最大尺寸不大于给定值
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [200,200]  #会对图片从中间进行裁剪
   end
 
   version :medium do
     process resize_to_fill: [400,400]
   end
+
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
