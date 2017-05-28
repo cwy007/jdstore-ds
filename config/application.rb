1167cf6ccef88bb7e9c465bacb30f75327e83d33
chanweiyan <chanweiyan007@gmail.com>
2017-05-28 16:41:53 +0800

    config i18n default_locale = zh-CN

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Jdstore
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Beijing"
+    config.i18n.default_locale = "zh-CN"
   end
 end
 
